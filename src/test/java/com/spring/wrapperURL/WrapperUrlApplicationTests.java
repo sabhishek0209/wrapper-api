20a9c3605d3f75069d837b641e3fe4ed737edc32
sabhishek0209 <2017012@iiitdmj.ac.in>
2024-10-09 20:16:32 +0530

    initial commit

---
+++ src/test/java/com/spring/wrapperURL/WrapperUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.wrapperURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapperUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
